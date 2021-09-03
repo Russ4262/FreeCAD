13c8aaecf1be046ba88d3f818635f574e0c61377
Russell Johnson <47639332+Russ4262@users.noreply.github.com>
2021-09-03 07:52:58 -0500

    Path: Downgrade debug level

--- src/Mod/Path/PathScripts/Strategies/PathStrategyProfile.py
+++ src/Mod/Path/PathScripts/Strategies/PathStrategyProfile.py
@@ -31,9 +31,8 @@ from PySide import QtCore
 
 # lazily loaded modules
 from lazy_loader.lazy_loader import LazyLoader
-
-Part = LazyLoader("Part", globals(), "Part")
-PathUtils = LazyLoader("PathScripts.PathUtils", globals(), "PathScripts.PathUtils")
+Part = LazyLoader('Part', globals(), 'Part')
+PathUtils = LazyLoader('PathScripts.PathUtils', globals(), 'PathScripts.PathUtils')
 # area = LazyLoader('area', globals(), 'area')
 
 
@@ -44,7 +43,7 @@ __doc__ = "Path strategies available for path generation."
 __contributors__ = ""
 
 
-PathLog.setLevel(PathLog.Level.DEBUG, PathLog.thisModule())
+PathLog.setLevel(PathLog.Level.INFO, PathLog.thisModule())
 # PathLog.trackModule(PathLog.thisModule())
 
 
@@ -54,31 +53,29 @@ def translate(context, text, disambig=None):
 
 
 class StrategyProfile:
-    """class StrategyProfile"""
+    '''class StrategyProfile'''
 
-    def __init__(
-        self,
-        shape,
-        isHole,
-        startPoint,
-        getsim,
-        depthparams,
-        horizFeed,
-        vertFeed,
-        endVector,
-        radius,
-        opFeatures,
-        safeHeight,
-        clearanceHeight,
-        stockAllowance,
-        direction,
-        cutSide,
-        useToolCompensation,
-        joinType,
-        miterLimit,
-        opUseProjection,
-        opRetractTool,
-    ):
+    def __init__(self,
+                shape,
+                isHole,
+                startPoint,
+                getsim,
+                depthparams,
+                horizFeed,
+                vertFeed,
+                endVector,
+                radius,
+                opFeatures,
+                safeHeight,
+                clearanceHeight,
+                stockAllowance,
+                direction,
+                cutSide,
+                useToolCompensation,
+                joinType,
+                miterLimit,
+                opUseProjection,
+                opRetractTool):
         PathLog.debug("StrategyProfile.__init__()")
 
         self.opUseProjection = opUseProjection
@@ -106,7 +103,7 @@ class StrategyProfile:
         self.areaParams = ""
         self.commandList = list()
 
-        """
+        '''
         @Sliptonic summation per chat on 7 June 2021
         def generateProfile (
                 face,
@@ -121,35 +118,35 @@ class StrategyProfile:
                 miterLimit=somedefault,
                 opRetractTool  # research / rename
                 ):
-        """
+        '''
 
     # Private methods
     def _getAreaParams(self):
-        """_getAreaParams() ... returns dictionary with area parameters."""
+        '''_getAreaParams() ... returns dictionary with area parameters.'''
         params = {}
-        params["Fill"] = 0
-        params["Coplanar"] = 0
-        params["SectionCount"] = -1
+        params['Fill'] = 0
+        params['Coplanar'] = 0
+        params['SectionCount'] = -1
 
         offset = self.stockAllowance  # 0.0
         if self.useToolCompensation:
             offset = self.radius + self.stockAllowance
-        if self.cutSide == "Inside":
+        if self.cutSide == 'Inside':
             offset = 0 - offset
         if self.isHole:
             offset = 0 - offset
-        params["Offset"] = offset
+        params['Offset'] = offset
 
-        jointype = ["Round", "Square", "Miter"]
-        params["JoinType"] = jointype.index(self.joinType)
+        jointype = ['Round', 'Square', 'Miter']
+        params['JoinType'] = jointype.index(self.joinType)
 
-        if self.joinType == "Miter":
-            params["MiterLimit"] = self.miterLimit
+        if self.joinType == 'Miter':
+            params['MiterLimit'] = self.miterLimit
 
         return params
 
     def _getPathParams(self):
-        """_getPathParams() ... returns dictionary with path parameters."""
+        '''_getPathParams() ... returns dictionary with path parameters.'''
         params = {}
 
         # Reverse the direction for holes
@@ -158,104 +155,86 @@ class StrategyProfile:
         else:
             direction = self.direction
 
-        orientation = ["Conventional", "Climb"]
-        params["orientation"] = orientation.index(self.direction)
+        orientation = ['Conventional', 'Climb']
+        params['orientation'] = orientation.index(self.direction)
 
         if not self.useToolCompensation:
-            if direction == "Conventional":
-                params["orientation"] = 1
+            if direction == 'Conventional':
+                params['orientation'] = 1
             else:
-                params["orientation"] = 0
+                params['orientation'] = 0
 
         return params
 
     # Public methods
     def generateCommands(self):
-        """generateCommands() ... public function to generate gcode for path area shape."""
+        '''generateCommands() ... public function to generate gcode for path area shape.'''
         PathLog.debug("StrategyProfile.generateCommands()")
 
         area = Path.Area()
         area.setPlane(PathUtils.makeWorkplane(self.shape))
         area.add(self.shape)
 
-        areaParams = self._getAreaParams()  # pylint: disable=assignment-from-no-return
+        areaParams = self._getAreaParams() # pylint: disable=assignment-from-no-return
 
         heights = [i for i in self.depthparams]
-        PathLog.debug("depths: {}".format(heights))
+        PathLog.debug('depths: {}'.format(heights))
         area.setParams(**areaParams)
         self.areaParams = str(area.getParams())
 
         PathLog.debug("Area with params: {}".format(area.getParams()))
 
-        sections = area.makeSections(
-            mode=0, project=self.opUseProjection, heights=heights
-        )
+        sections = area.makeSections(mode=0, project=self.opUseProjection, heights=heights)
         PathLog.debug("sections = %s" % sections)
         shapelist = [sec.getShape() for sec in sections]
         PathLog.debug("shapelist = %s" % shapelist)
 
-        pathParams = self._getPathParams()  # pylint: disable=assignment-from-no-return
-        pathParams["shapes"] = shapelist
-        pathParams["feedrate"] = self.horizFeed
-        pathParams["feedrate_v"] = self.vertFeed
-        pathParams["verbose"] = True
-        pathParams["resume_height"] = self.resume_height
-        pathParams["retraction"] = self.retraction
-        pathParams["return_end"] = True
+        pathParams = self._getPathParams() # pylint: disable=assignment-from-no-return
+        pathParams['shapes'] = shapelist
+        pathParams['feedrate'] = self.horizFeed
+        pathParams['feedrate_v'] = self.vertFeed
+        pathParams['verbose'] = True
+        pathParams['resume_height'] = self.resume_height
+        pathParams['retraction'] = self.retraction
+        pathParams['return_end'] = True
         # Note that emitting preambles between moves breaks some dressups and prevents path optimization on some controllers
-        pathParams["preamble"] = False
+        pathParams['preamble'] = False
 
         # if not self.opRetractTool:
         #    pathParams['threshold'] = 2.001 * self.radius
 
         if self.endVector is not None:
-            pathParams["start"] = self.endVector
+            pathParams['start'] = self.endVector
         elif self.startPoint:
-            pathParams["start"] = self.startPoint
+            pathParams['start'] = self.startPoint
 
-        self.pathParams = str(
-            {key: value for key, value in pathParams.items() if key != "shapes"}
-        )
+        self.pathParams = str({key: value for key, value in pathParams.items() if key != 'shapes'})
         PathLog.debug("Path with params: {}".format(self.pathParams))
 
         (pp, end_vector) = Path.fromShapes(**pathParams)
-        PathLog.debug("pp: {}, end vector: {}".format(pp, end_vector))
-        self.endVector = end_vector  # pylint: disable=attribute-defined-outside-init
+        PathLog.debug('pp: {}, end vector: {}'.format(pp, end_vector))
+        self.endVector = end_vector # pylint: disable=attribute-defined-outside-init
 
         if self.getsim:
-            areaParams["Thicken"] = True
-            areaParams["ToolRadius"] = self.radius - self.radius * 0.005
+            areaParams['Thicken'] = True
+            areaParams['ToolRadius'] = self.radius - self.radius * .005
             area.setParams(**areaParams)
-            sec = area.makeSections(mode=0, project=False, heights=heights)[
-                -1
-            ].getShape()
+            sec = area.makeSections(mode=0, project=False, heights=heights)[-1].getShape()
             self.simObj = sec.extrude(FreeCAD.Vector(0, 0, self.shape.BoundBox.ZMax))
 
         self.commandList = pp.Commands
 
     def getSimObj(self):
         return self.simObj
-
-
 # Eclass
 
 
 class StrategyProfileOpenEdge:
-    """class StrategyProfileOpenEdge"""
+    '''class StrategyProfileOpenEdge'''
 
-    def __init__(
-        self,
-        edgeList,
-        startPoint,
-        depthparams,
-        horizFeed,
-        vertFeed,
-        endVector,
-        radius,
-        safeHeight,
-        clearanceHeight,
-        direction,
-    ):
+    def __init__(self, edgeList, startPoint, depthparams,
+                 horizFeed, vertFeed, endVector, radius,
+                 safeHeight, clearanceHeight, direction):
         PathLog.debug("StrategyProfileOpenEdge.__init__()")
 
         self.edgeList = edgeList
@@ -275,12 +254,12 @@ class StrategyProfileOpenEdge:
 
     # Public methods
     def generateCommands(self):
-        """generateCommands() ... internal function."""
+        '''generateCommands() ... internal function.'''
         PathLog.debug("StrategyProfileOpenEdge.generateCommands()")
 
         commands = []
         heights = [i for i in self.depthparams]
-        PathLog.debug("depths: {}".format(heights))
+        PathLog.debug('depths: {}'.format(heights))
         end_vector = None
         cntHeights = len(heights)
 
@@ -292,41 +271,35 @@ class StrategyProfileOpenEdge:
                 hWire = Part.Wire(Part.__sortEdges__(wire.Edges))
                 hWire.translate(FreeCAD.Vector(0, 0, heights[i] - hWire.BoundBox.ZMin))
 
-                pathParams = {}  # pylint: disable=assignment-from-no-return
-                pathParams["shapes"] = [hWire]
-                pathParams["feedrate"] = self.horizFeed
-                pathParams["feedrate_v"] = self.vertFeed
-                pathParams["verbose"] = True
-                pathParams["resume_height"] = self.resume_height
-                pathParams["retraction"] = self.retraction
-                pathParams["return_end"] = True
+                pathParams = {} # pylint: disable=assignment-from-no-return
+                pathParams['shapes'] = [hWire]
+                pathParams['feedrate'] = self.horizFeed
+                pathParams['feedrate_v'] = self.vertFeed
+                pathParams['verbose'] = True
+                pathParams['resume_height'] = self.resume_height
+                pathParams['retraction'] = self.retraction
+                pathParams['return_end'] = True
                 # Note that emitting preambles between moves breaks some dressups and prevents path optimization on some controllers
-                pathParams["preamble"] = False
+                pathParams['preamble'] = False
 
                 if True or self.endVector is None:
                     vrtxs = hWire.Wires[0].Vertexes
                     lv = len(vrtxs) - 1
-                    pathParams["start"] = FreeCAD.Vector(
-                        vrtxs[0].X, vrtxs[0].Y, vrtxs[0].Z
-                    )
-                    if self.direction == "Conventional":
-                        pathParams["start"] = FreeCAD.Vector(
-                            vrtxs[lv].X, vrtxs[lv].Y, vrtxs[lv].Z
-                        )
+                    pathParams['start'] = FreeCAD.Vector(vrtxs[0].X, vrtxs[0].Y, vrtxs[0].Z)
+                    if self.direction == 'Conventional':
+                        pathParams['start'] = FreeCAD.Vector(vrtxs[lv].X, vrtxs[lv].Y, vrtxs[lv].Z)
                     if not start:
-                        start = pathParams["start"]
+                        start = pathParams['start']
                 else:
-                    pathParams["start"] = self.endVector
+                    pathParams['start'] = self.endVector
 
-                self.pathParams = str(
-                    {key: value for key, value in pathParams.items() if key != "shapes"}
-                )
+                self.pathParams = str({key: value for key, value in pathParams.items() if key != 'shapes'})
                 PathLog.debug("Path with params: {}".format(self.pathParams))
 
                 (pp, end_vector) = Path.fromShapes(**pathParams)
                 commands.extend(pp.Commands)
                 commands.append(Path.Command("G0", {"Z": self.resume_height}))
-                PathLog.debug("pp: {}, end vector: {}".format(pp, end_vector))
+                PathLog.debug('pp: {}, end vector: {}'.format(pp, end_vector))
             # Efor
             if i < cntHeights - 1:
                 commands.append(Path.Command("G0", {"X": start.x, "Y": start.y}))
@@ -334,6 +307,4 @@ class StrategyProfileOpenEdge:
         self.endVector = end_vector
 
         self.commandList = commands
-
-
 # Eclass
