9583e7c6c50a92cc0231ca6a26f71e822b04e51c
Russell Johnson <47639332+Russ4262@users.noreply.github.com>
2022-03-17 20:36:55 -0500

    Path: Fix Job creation without task panel interaction
    This fix allows for Job creation with view provider, but without initial task panel interaction.

--- src/Mod/Path/PathScripts/PathJobGui.py
+++ src/Mod/Path/PathScripts/PathJobGui.py
@@ -1583,7 +1583,7 @@ class TaskPanel:
         self.updateSelection()
 
 
-def Create(base, template=None):
+def Create(base, template=None, useGui=True):
     """Create(base, template) ... creates a job instance for the given base object
     using template to configure it."""
     FreeCADGui.addModule("PathScripts.PathJob")
@@ -1594,7 +1594,12 @@ def Create(base, template=None):
         obj.ViewObject.addExtension("Gui::ViewProviderGroupExtensionPython")
         FreeCAD.ActiveDocument.commitTransaction()
         obj.Document.recompute()
-        obj.ViewObject.Proxy.editObject(obj.Stock)
+
+        if useGui:
+            obj.ViewObject.Proxy.editObject(obj.Stock)
+        else:
+            obj.ViewObject.Proxy.deleteOnReject = False
+
         return obj
     except Exception as exc:
         PathLog.error(exc)
